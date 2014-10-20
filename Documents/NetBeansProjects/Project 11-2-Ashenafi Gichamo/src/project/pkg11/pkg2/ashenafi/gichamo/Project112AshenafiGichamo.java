3400daf339c8a325fe31c9a76f30e0919416c14d
Ash <agichamo13@apu.edu>
2014-10-20 15:36:10 -0700

    Project 11-2: Display a sorted list of student scores has been instantiated :)

---
+++ Documents/NetBeansProjects/Project 11-2-Ashenafi Gichamo/src/project/pkg11/pkg2/ashenafi/gichamo/Project112AshenafiGichamo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.pkg11.pkg2.ashenafi.gichamo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author agichamo
+ */
+public class Project112AshenafiGichamo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    
+    {
+        // TODO code application logic here
+        System.out.println("Welcome to the Student Scores Application.");
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("Enter number of students to enter: ");
+        int numberofStudent = sc.nextInt();
+        
+        
+    }
+    
+}
---
+++ Documents/NetBeansProjects/Project 11-2-Ashenafi Gichamo/src/project/pkg11/pkg2/ashenafi/gichamo/Student.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.pkg11.pkg2.ashenafi.gichamo;
+
+/**
+ *
+ * @author agichamo
+ */
+public class Student implements comparable {
+    
+}
